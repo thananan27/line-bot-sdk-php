d25515274778fa1ef53bf7e6d378aa581fe19a68
thananan27 <thananan27@gmail.com>
2018-09-20 23:36:00 +0700

    Update echo_bot.php

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -18,8 +18,7 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = 'z+NI9cKbWu9wprSHuvta4EV+kfMhnintttWNxhYyErPoIvVOP/2yw7Zt2OS6JxI78U1/jn2reZAyutSTuGAPLOvfLg3jlakTRw8cb20yAVXrvs0RqYFzarIVHXA533mGLs43tttDzr0XlPk4ChldqwdB04t89/1O/w1cDnyilFU=
-';
+$channelAccessToken = 'z+NI9cKbWu9wprSHuvta4EV+kfMhnintttWNxhYyErPoIvVOP/2yw7Zt2OS6JxI78U1/jn2reZAyutSTuGAPLOvfLg3jlakTRw8cb20yAVXrvs0RqYFzarIVHXA533mGLs43tttDzr0XlPk4ChldqwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'fd7e04d93f360ae5fc89c96a5a148259';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
